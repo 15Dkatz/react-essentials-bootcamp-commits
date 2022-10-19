d8399e12fcb25bd9908e80bd5b1fe2543c8b07c2
David Katz <15Dkatz@shcp.edu>
2022-10-18 17:21:20 -0700

    State

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
-class RegularClass {}
-class ComponentClass extends Component {}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: true };
+  }
+
   render() {
     return (
       <div>
         <h1>Hello!</h1>
         <p>My name is David. I'm a software engineer.</p>
         <p>I'm always looking forward to work on meaningful projects.</p>
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in San Francisco, and code every day.</p>
+              <p>My favorite language is JavaScript, and I think React.js is awesome</p>
+              <p>Besides coding, I also love music and ramen!</p>
+            </div>
+          ) : null
+        }
       </div>
     ); 
   }
