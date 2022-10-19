0cc0f8cf8bf85d689a311a57a6ba318ab0cbb538
David Katz <15Dkatz@shcp.edu>
2022-10-18 17:18:16 -0700

    A React Component - App

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is David. I'm a software engineer.</p>
+        <p>I'm always looking forward to work on meaningful projects.</p>
+      </div>
+    ); 
+  }
+}
+
+export default App;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>React element</div>);
+root.render(<App />);
