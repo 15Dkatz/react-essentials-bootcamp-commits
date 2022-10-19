bae8de13c8142ac5a1c395e018b4c70f010ca02f
David Katz <15Dkatz@shcp.edu>
2022-10-18 22:39:55 -0700

    Compiling, Bundling, and a Closer Look at JSX

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 console.log('hi');
 
-const element = React.createElement('div', null, 'React element!');
+const element = <div>React element with JSX!</div>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
