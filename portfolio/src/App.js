2c3038d9a508e4571db8da501075e778debe73af
David Katz <15Dkatz@shcp.edu>
2022-10-18 17:20:32 -0700

    Classes, Inheritance, and a Closer Component Look

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+class RegularClass {}
+class ComponentClass extends Component {}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
 class App extends Component {
   render() {
     return (
