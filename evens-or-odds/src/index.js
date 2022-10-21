ea82795fcedcb0ecb592ef8efa13ee9c4bbaf81a
David Katz <15Dkatz@shcp.edu>
2022-10-21 15:57:01 -0700

    Actions

--- evens-or-odds/src/index.js
+++ evens-or-odds/src/index.js
@@ -9,15 +9,32 @@ const DEFAULT_SETTINGS = {
   instructionsExpanded: false
 };
 
-const rootReducer = () => {
-  return DEFAULT_SETTINGS;
+const rootReducer = (state = DEFAULT_SETTINGS, action) => {
+  console.log('state', state, 'action', action);
+
+  if (action.type === 'SET_GAME_STARTED') {
+    return {
+      gameStarted: action.gameStarted,
+      instructionsExpanded: false
+    }
+  }
+
+  return state;
 };
 
 const store = configureStore({
   reducer: rootReducer
 });
 console.log('store', store);
-console.log('store.getState()', store.getState());
+store.subscribe(() => {
+  console.log('store.getState()', store.getState());
+});
+
+const action1 = { type: 'SET_GAME_STARTED', gameStarted: true };
+store.dispatch(action1);
+
+store.dispatch({ type: 'foo' });
+store.dispatch({ type: 'bar' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
