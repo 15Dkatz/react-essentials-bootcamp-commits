13cf5393393d1c2cf2370415a9a48579216e2a53
David Katz <15Dkatz@shcp.edu>
2022-10-21 17:00:24 -0700

    Connect React Components to Redux

--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('this', this);
+
     return (
       <div>React App</div>
     ); 
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log('state', state);
+
+  return { gameStarted: state.gameStarted };
+}
+
+const componentConnector = connect(mapStateToProps);
+
+export default componentConnector(App);
--- evens-or-odds/src/index.js
+++ evens-or-odds/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
@@ -13,4 +14,8 @@ store.subscribe(() => {
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
