ec34051f32cbb90a43a4c10d981d7e8c02a9a159
David Katz <15Dkatz@shcp.edu>
2022-10-24 11:48:39 -0700

    Challenge and Code: Card Component

--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -5,6 +5,7 @@ import { fetchNewDeck } from '../actions/deck';
 import fetchStates from '../reducers/fetchStates';
 import Instructions from './Instructions';
 import DrawCard from './DrawCard';
+import Card from './Card';
 
 class App extends Component {
   startGame = () => {
@@ -33,6 +34,8 @@ class App extends Component {
               <h3>The game is on!</h3>
               <br />
               <DrawCard />
+              <hr />
+              <Card />
               <hr />
               <button onClick={this.props.cancelGame}>Cancel Game</button>
             </div>
