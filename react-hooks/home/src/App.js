162aae7cab209722e73a1186646dc6d1ee9508b0
David Katz <15Dkatz@shcp.edu>
2022-10-25 13:28:05 -0700

    First Hook: UseState

--- react-hooks/home/src/App.js
+++ react-hooks/home/src/App.js
@@ -1,6 +1,36 @@
+import { useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">App</div>
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+
+    setUserQuery(event.target.value);
+  }
+
+  return ( 
+    <div className="App">
+      <h1>Hello David</h1>
+      <div className='form'>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+    </div>
   );
 }
 
