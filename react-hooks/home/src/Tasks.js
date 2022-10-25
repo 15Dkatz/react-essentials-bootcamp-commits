035dc17d1a79ccd9128536fad0234f9f147973c3
David Katz <15Dkatz@shcp.edu>
2022-10-25 14:42:50 -0700

    Tasks Component Part 2

--- react-hooks/home/src/Tasks.js
+++ react-hooks/home/src/Tasks.js
@@ -14,7 +14,17 @@ function Tasks() {
     setTasks([...tasks, { taskText, id: uuid() }]);
   }
 
+  const completeTask = completedTask => () => {
+    setCompletedTasks([...completedTasks, completedTask]);
+    setTasks(tasks.filter(task => task.id !== completedTask.id));
+  }
+
+  const deleteTask = task => () => {
+    setCompletedTasks(completedTasks.filter(t => t.id !== task.id));
+  }
+
   console.log('tasks', tasks);
+  console.log('completedTasks', completedTasks);
 
   return (
     <div>
@@ -28,7 +38,25 @@ function Tasks() {
           tasks.map(task => {
             const { id, taskText } = task;
 
-            return <div key={id}>{taskText}</div>
+            return (
+              <div key={id} onClick={completeTask(task)}>
+                {taskText}
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='completed-list'>
+        {
+          completedTasks.map(task => {
+            const { id, taskText } = task;
+
+            return (
+              <div key={id}>
+                {taskText}{' '}
+                <span onClick={deleteTask(task)} className='delete-task'>x</span>
+              </div>
+            );
           })
         }
       </div>
