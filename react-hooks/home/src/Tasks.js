0365754620aebe0bb14974bdf4e26b66685a1f9e
David Katz <15Dkatz@shcp.edu>
2022-10-25 14:50:23 -0700

    Local Storage for Tasks

--- react-hooks/home/src/Tasks.js
+++ react-hooks/home/src/Tasks.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v4';
+
+const TASKS_STORAGE_KEY = 'TASKS_STORAGE_KEY';
+
+const storeTasks = (taskMap) => {
+  localStorage.setItem(
+    TASKS_STORAGE_KEY,
+    JSON.stringify(taskMap)
+  );
+}
+
+const readStoredTasks = () => {
+  const tasksMap = JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY));
+
+  return tasksMap ? tasksMap : { tasks: [], completedTasks: [] };
+}
 
 function Tasks() {
   const [taskText, setTaskText] = useState('');
-  const [tasks, setTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
+  const storedTasks = readStoredTasks();
+  const [tasks, setTasks] = useState(storedTasks.tasks);
+  const [completedTasks, setCompletedTasks] = useState(storedTasks.completedTasks);
+
+  useEffect(() => {
+    storeTasks({ tasks, completedTasks });
+  });
 
   const updateTaskText = event => {
     setTaskText(event.target.value);
