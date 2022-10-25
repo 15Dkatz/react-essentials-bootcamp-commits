f9d60b498ee7e18690a1c2dc4e2bd2cac9ed52ad
David Katz <15Dkatz@shcp.edu>
2022-10-25 15:09:14 -0700

    Dynamic Delay and Increment

--- react-hooks/home/src/Gallery.js
+++ react-hooks/home/src/Gallery.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import PICTURES from './data/pictures';
 
+const SECONDS = 1000;
+const minimumDelay = 1 * SECONDS;
+const minimumIncrement = 1;
+
 function Gallery() {
   const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
 
   useEffect(() => {
+    console.log('delay', delay, 'increment', increment);
+
     const interval = setInterval(() => {
       setIndex(
         storedIndex => {
-          return (storedIndex+1)%PICTURES.length;
+          return (storedIndex+increment)%PICTURES.length;
         }
       );
-    }, 3000);
+    }, delay);
 
     return () => {
+      console.log('remove last interval');
       clearInterval(interval);
     }
-  }, []);
+  }, [delay, increment]);
 
   console.log('index', index);
+
+  const updateDelay = event => {
+    const delay = Number(event.target.value) * SECONDS;
+
+    setDelay(delay < minimumDelay ? minimumDelay : delay);
+  }
+
+  const updateIncrement = event => {
+    const increment = Number(event.target.value);
+
+    setIncrement(increment < minimumIncrement ? minimumIncrement : increment);
+  }
 
   return (
     <div className='Gallery'>
@@ -26,6 +47,16 @@ function Gallery() {
         src={PICTURES[index].image}
         alt='gallery'
       />
+      <div className='multiform'>
+        <div>
+          Gallery transition delay (seconds):
+          <input type='number' onChange={updateDelay} />
+        </div>
+        <div>
+          Gallery increment:
+          <input type='number' onChange={updateIncrement} />
+        </div>
+      </div>
     </div>
   )
 }
